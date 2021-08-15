35f8378702e959fb951de7e1aa48890b3c4c52c7
alochym <alochym@gmail.com>
2021-08-15 17:49:59 +0700

    using sqlite

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,77 +1,102 @@
 package main
 
 import (
-	"github.com/alochym01/gin-website/api"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-// type album struct {
-// 	ID     string  `json:"id"`
-// 	Title  string  `json:"title"`
-// 	Artist string  `json:"artist"`
-// 	Price  float64 `json:"price"`
-// }
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
 
 // albums slice to seed record album data.
-// var albums = []album{
-// 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-// 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 117.99},
-// 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-// }
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
 
 func main() {
-	router := api.Router()
-	// router := gin.Default()
+	os.Remove("foo.db")
 
-	// // Using anonymous function
-	// router.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(http.StatusOK, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
+	// sqlite section
+	db, _ := sql.Open("sqlite3", "./foo.db")
+	defer db.Close()
 
-	// // Using normal function
-	// router.GET("/albums", api.getAlbums)
-	// router.GET("/albums/:id", getAlbumByID)
-	// router.POST("/albums", postAlbums)
+	// Create Album table
+	Album := `CREATE TABLE albums (
+		"id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"title" TEXT,
+		"artist" TEXT,
+		"price" TEXT
+	  );` // SQL Statement for Create Table
 
-	// A expvar handler for gin framework
+	log.Println("Create student table...")
+	statement, err := db.Prepare(Album) // Prepare SQL Statement
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec() // Execute SQL Statements
+	log.Println("Album table created")
+
+	// Prepare to insert value
+	stmt, _ := db.Prepare("INSERT INTO albums(title, artist, price) values(?,?,?)")
+
+	for _, v := range albums {
+		fmt.Println(v.ID)
+		fmt.Println(v.Artist)
+		fmt.Println(v.Title)
+		fmt.Println(v.Price)
+
+		// fmt.Println(stmt)
+		_, _ = stmt.Exec(v.Title, v.Artist, v.Price)
+	}
+
+	router := gin.Default()
+
+	// Using anonymous function
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// Using normal function
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+
 	router.Run()
 }
 
-// // getAlbums responds with the list of all albums as JSON.
-// func getAlbums(c *gin.Context) {
-// 	c.IndentedJSON(http.StatusOK, entity.Albums)
-// }
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
 
-// // postAlbums adds an album from JSON received in the request body.
-// func postAlbums(c *gin.Context) {
-// 	var newAlbum entity.Album
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
 
-// 	// Call BindJSON to bind the received JSON to newAlbum.
-// 	if err := c.BindJSON(&newAlbum); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{
-// 			"message": "Invalid Params",
-// 		})
-// 		return
-// 	}
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid Params",
+		})
+		return
+	}
 
-// 	// Add the new album to the slice.
-// 	entity.Albums = append(entity.Albums, newAlbum)
-// 	c.IndentedJSON(http.StatusCreated, newAlbum)
-// }
-
-// // getAlbumByID locates the album whose ID value matches the id
-// // parameter sent by the client, then returns that album as a response.
-// func getAlbumByID(c *gin.Context) {
-// 	id := c.Param("id")
-
-// 	// Loop over the list of albums, looking for
-// 	// an album whose ID value matches the parameter.
-// 	for _, a := range entity.Albums {
-// 		if a.ID == id {
-// 			c.IndentedJSON(http.StatusOK, a)
-// 			return
-// 		}
-// 	}
-// 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-// }
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
