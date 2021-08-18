83a6f5e9cabfcb5af96552c89931da81aae4fe3e
alochym <alochym@gmail.com>
2021-08-18 22:49:14 +0700

    use custom log format

--- router/router.go
+++ router/router.go
@@ -1,16 +1,37 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/alochym01/gin-website/handler"
 	"github.com/gin-gonic/gin"
 )
 
+// Router is returned *gin.Engine
 func Router() *gin.Engine {
+	// Disable log's color
+	gin.DisableConsoleColor()
 
-	router := gin.Default()
+	router := gin.New()
 
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\" \n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	router.Use(gin.Recovery())
 	// Using anonymous function
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
